302011eca1c74dc4e10a1ccb1ca8f168a61560af
Kartik Mittal <mittal38@purdue.edu>
2020-10-01 01:41:41 -0400

    ANN implemented

--- Lab2.py
+++ Lab2.py
@@ -1,4 +1,3 @@
-
 import os
 import numpy as np
 import tensorflow as tf
@@ -6,24 +5,23 @@ from tensorflow import keras
 from tensorflow.keras.utils import to_categorical
 import random
 
-
 random.seed(1618)
 np.random.seed(1618)
-#tf.set_random_seed(1618)   # Uncomment for TF1.
+# tf.set_random_seed(1618)   # Uncomment for TF1.
 tf.random.set_seed(1618)
 
-#tf.logging.set_verbosity(tf.logging.ERROR)   # Uncomment for TF1.
+# tf.logging.set_verbosity(tf.logging.ERROR)   # Uncomment for TF1.
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-ALGORITHM = "guesser"
-#ALGORITHM = "tf_net"
-#ALGORITHM = "tf_conv"
+# ALGORITHM = "guesser"
+ALGORITHM = "tf_net"
+# ALGORITHM = "tf_conv"
 
 DATASET = "mnist_d"
-#DATASET = "mnist_f"
-#DATASET = "cifar_10"
-#DATASET = "cifar_100_f"
-#DATASET = "cifar_100_c"
+# DATASET = "mnist_f"
+# DATASET = "cifar_10"
+# DATASET = "cifar_100_f"
+# DATASET = "cifar_100_c"
 
 if DATASET == "mnist_d":
     NUM_CLASSES = 10
@@ -38,14 +36,16 @@ elif DATASET == "mnist_f":
     IZ = 1
     IS = 784
 elif DATASET == "cifar_10":
-    pass                                 # TODO: Add this case.
+    pass  # TODO: Add this case.
 elif DATASET == "cifar_100_f":
-    pass                                 # TODO: Add this case.
+    pass  # TODO: Add this case.
 elif DATASET == "cifar_100_c":
-    pass                                 # TODO: Add this case.
+    pass  # TODO: Add this case.
+
+NEURONS = 512
 
 
-#=========================<Classifier Functions>================================
+# =========================<Classifier Functions>================================
 
 def guesserClassifier(xTest):
     ans = []
@@ -56,16 +56,90 @@ def guesserClassifier(xTest):
     return np.array(ans)
 
 
-def buildTFNeuralNet(x, y, eps = 6):
-    pass        #TODO: Implement a standard ANN here.
+class NeuralNetwork_2Layer():
+    def __init__(self, inputSize, outputSize, neuronsPerLayer, learningRate=0.05):
+        self.inputSize = inputSize
+        self.outputSize = outputSize
+        self.neuronsPerLayer = neuronsPerLayer
+        self.lr = learningRate
+        self.W1 = np.random.randn(self.inputSize, self.neuronsPerLayer)
+        self.W2 = np.random.randn(self.neuronsPerLayer, self.outputSize)
+
+    # Activation function.
+    def __sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))  # TODO: implement
+
+    # Activation prime function.
+    def __sigmoidDerivative(self, x):
+        sig = self.__sigmoid(x)
+        der = sig * (1 - sig)
+        return der  # TODO: implement
+
+    # Batch generator for mini-batches. Not randomized.
+    def __batchGenerator(self, l, n):
+        for i in range(0, len(l), n):
+            yield l[i: i + n]
+
+    # Training with backpropagation.
+    # TODO: Implement backprop. allow minibatches. mbs should specify the size of each minibatch.
+    def train(self, xVals, yVals, epochs=5, minibatches=True, mbs=100):
+        num_batches = xVals.shape[0] / mbs
+        xValBatches = np.split(xVals, num_batches)
+        yValBatches = np.split(yVals, num_batches)
+        for i in range(epochs):
+            for j in range(num_batches):
+                layer1, layer2 = self.__forward(xValBatches[j])
+                L2e = (layer2 - yValBatches[j])
+
+                sig_der_layer2 = self.__sigmoidDerivative(layer2)
+                L2d = L2e * sig_der_layer2
+
+                L1e = np.dot(L2d, self.W2.T)
+
+                sig_der_layer1 = self.__sigmoidDerivative(layer1)
+                L1d = L1e * sig_der_layer1
+
+                L1a = (np.dot(xValBatches[j].T, L1d)) * self.lr
+                L2a = (np.dot(layer1.T, L2d)) * self.lr
+
+                self.W1 -= L1a
+                self.W2 -= L2a
+
+        return self
+
+    # Forward pass.
+    def __forward(self, input):
+        layer1 = self.__sigmoid(np.dot(input, self.W1))
+        layer2 = self.__sigmoid(np.dot(layer1, self.W2))
+        return layer1, layer2
+
+    # Predict.
+    def predict(self, xVals):
+        _, layer2 = self.__forward(xVals)
+        ans = []
+        for entry in layer2:
+            pred = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            index = entry.argmax()
+            pred[index] = 1
+            ans.append(pred)
+
+        return np.array(ans)
+
+
+def buildTFNeuralNet(x, y, eps=6):
+    # TODO: Implement a standard ANN here.
+
+    model = NeuralNetwork_2Layer(IS, NUM_CLASSES, NEURONS)
+
+    return model.train(x, y)
+
+
+def buildTFConvNet(x, y, eps=10, dropout=True, dropRate=0.2):
+    pass  # TODO: Implement a CNN here. dropout option is required.
     return None
 
 
-def buildTFConvNet(x, y, eps = 10, dropout = True, dropRate = 0.2):
-    pass        #TODO: Implement a CNN here. dropout option is required.
-    return None
-
-#=========================<Pipeline Functions>==================================
+# =========================<Pipeline Functions>==================================
 
 def getRawData():
     if DATASET == "mnist_d":
@@ -75,11 +149,11 @@ def getRawData():
         mnist = tf.keras.datasets.fashion_mnist
         (xTrain, yTrain), (xTest, yTest) = mnist.load_data()
     elif DATASET == "cifar_10":
-        pass      # TODO: Add this case.
+        pass  # TODO: Add this case.
     elif DATASET == "cifar_100_f":
-        pass      # TODO: Add this case.
+        pass  # TODO: Add this case.
     elif DATASET == "cifar_100_c":
-        pass      # TODO: Add this case.
+        pass  # TODO: Add this case.
     else:
         raise ValueError("Dataset not recognized.")
     print("Dataset: %s" % DATASET)
@@ -88,7 +162,6 @@ def getRawData():
     print("Shape of xTest dataset: %s." % str(xTest.shape))
     print("Shape of yTest dataset: %s." % str(yTest.shape))
     return ((xTrain, yTrain), (xTest, yTest))
-
 
 
 def preprocessData(raw):
@@ -108,11 +181,10 @@ def preprocessData(raw):
     return ((xTrainP, yTrainP), (xTestP, yTestP))
 
 
-
 def trainModel(data):
     xTrain, yTrain = data
     if ALGORITHM == "guesser":
-        return None   # Guesser has no model, as it is just guessing.
+        return None  # Guesser has no model, as it is just guessing.
     elif ALGORITHM == "tf_net":
         print("Building and training TF_NN.")
         return buildTFNeuralNet(xTrain, yTrain)
@@ -121,7 +193,6 @@ def trainModel(data):
         return buildTFConvNet(xTrain, yTrain)
     else:
         raise ValueError("Algorithm not recognized.")
-
 
 
 def runModel(data, model):
@@ -147,20 +218,18 @@ def runModel(data, model):
         raise ValueError("Algorithm not recognized.")
 
 
-
 def evalResults(data, preds):
     xTest, yTest = data
     acc = 0
     for i in range(preds.shape[0]):
         if np.array_equal(preds[i], yTest[i]):   acc = acc + 1
-    accuracy = acc / preds.shape[0]
+    accuracy = acc / (preds.shape[0] * 1.0)
     print("Classifier algorithm: %s" % ALGORITHM)
     print("Classifier accuracy: %f%%" % (accuracy * 100))
     print()
 
 
-
-#=========================<Main>================================================
+# =========================<Main>================================================
 
 def main():
     raw = getRawData()
@@ -168,7 +237,6 @@ def main():
     model = trainModel(data[0])
     preds = runModel(data[1][0], model)
     evalResults(data[1], preds)
-
 
 
 if __name__ == '__main__':
